1d09d8b82be21653641257488868a2b4f5f571d8
JhonMichelDiazDev <jhondiaz266@gmail.com>
2025-02-06 09:09:11 -0500

    Inicia Proyecto

---
+++ src/main/java/com/JMichelD/Proyecto_Reservas_Back/ProyectoReservasBackApplication.java
@@ -0,0 +1,13 @@
+package com.JMichelD.Proyecto_Reservas_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoReservasBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoReservasBackApplication.class, args);
+	}
+
+}
